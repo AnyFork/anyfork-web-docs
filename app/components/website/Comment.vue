3cc454346b0a60bd0c0d71d23dc794d7e76ea8ed
anyfork <anyfork@gmail.com>
2026-03-13 20:26:42 +0800

    fix(comment): :bug: 修复comment加载异常问题

--- app/components/website/Comment.vue
+++ app/components/website/Comment.vue
@@ -1,27 +1,29 @@
-<!-- eslint-disable vue/attribute-hyphenation -->
 <template>
-    <Waline :serverURL="runtimeConfig.public.NUXT_COMMENT_BASE_URL" :path="$route.path" lang="zh-CN" :dark="isDark" reaction :locale="locale" />
+    <div id="waline"></div>
 </template>
-
 <script setup lang="ts">
-    import { Waline } from '@waline/client/component'
+    import { init } from '@waline/client'
     import '@waline/client/style'
+
     const runtimeConfig = useRuntimeConfig()
-    const colorMode = useColorMode()
-    const isDark = computed({
-        get() {
-            return colorMode.value === 'dark'
-        },
-        set(_isDark) {
-            colorMode.preference = _isDark ? 'dark' : 'light'
-        }
+    const route = useRoute()
+    const path = computed(() => route.path)
+
+    onMounted(() => {
+        init({
+            el: '#waline',
+            serverURL: runtimeConfig.public.NUXT_COMMENT_BASE_URL,
+            path: path.value,
+            reaction: true,
+            locale: {
+                level0: '炼体',
+                level1: '炼气',
+                level2: '筑基',
+                level3: '金丹',
+                level4: '元婴',
+                level5: '化神'
+            },
+            lang: 'zh-CN'
+        })
     })
-    const locale = {
-        level0: '炼体',
-        level1: '炼气',
-        level2: '筑基',
-        level3: '金丹',
-        level4: '元婴',
-        level5: '化神'
-    }
 </script>
